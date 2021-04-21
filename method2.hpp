4d4cf76422f50de97de21a0ffcc3eafe8cefbebf
doug@luna <doug@totem9.net>
2021-04-21 22:13:21 +0100

    file cleanup

--- method2.hpp
+++ method2.hpp
@@ -201,3 +201,48 @@ void ps_block() {
 		else    inp.expect("'end")     ;
 		// else    break;
 }
+
+
+
+// ----------------------------------------
+// Expression parsing 
+// ----------------------------------------
+
+
+
+void pse_atom() {
+	if    (inp.get("@integer", r1))  outp.ex_push(r1.at(0));
+	else  ps_varpath();
+}
+
+void pse_mul() {
+	pse_atom();
+	if      (inp.get("'*"))  pse_mul(),  outp.ex_push("*");
+	else if (inp.get("'/"))  pse_mul(),  outp.ex_push("/");
+}
+void pse_add() {
+	pse_mul();
+	if      (inp.get("'+"))  pse_add(),  outp.ex_push("+");
+	else if (inp.get("'-"))  pse_add(),  outp.ex_push("-");
+}
+void pse_compare() {
+	pse_add();
+	if (	inp.get("'==", r1) || inp.get("'!=", r1) 
+			|| inp.get("'>=", r1) || inp.get("'<=", r1) 
+			|| inp.get("'<", r1) || inp.get("'>", r1)
+		) {
+		string op = r1.at(0);
+		pse_compare();
+		outp.ex_push(op);
+	}
+}
+
+// TODO: 
+//		and/or
+// 		brackets
+
+void ps_expression() {
+	outp.ex_start();
+	pse_compare();
+	outp.ex_end();
+}
---
+++ output.hpp
@@ -0,0 +1,113 @@
+#pragma once
+
+// ASM Output
+struct Output {
+	vector<string> output;
+	void string_literal(const string& literal) {
+		printf("	literal: [%s]\n", literal.c_str());
+	}
+	void struct_start(const string& id) {
+		printf("struct name: [%s]\n", id.c_str());
+	}
+	// void struct_addmember(const string& type, const string& id, bool isarray) {
+	// 	printf("	member: [%s] [%s] %s\n", type.c_str(), id.c_str(), (isarray ? "array" : ""));
+	// }
+	void struct_end() {
+		printf("	struct end\n");
+	}
+	void dim_start(const string& type, const string& id) {
+		printf("dim [%s] as [%s]\n", type.c_str(), id.c_str());
+	}
+	void dim_isarray(bool val) {
+		printf("	array [%d]\n", 0);
+	}
+	void dim_end() {
+		printf("	dim end\n");
+	}
+	void dim_short(const string& type, const string& id, bool isarray) {
+		printf("	member: [%s] [%s] %s\n", type.c_str(), id.c_str(), (isarray ? "array" : ""));
+	}
+	void func_start(const string& id) {
+		printf("function start: [%s]\n", id.c_str());
+	}
+	// void func_arg(const string& type, const string& id, bool isarray) {
+	// 	printf("	arg: [%s] [%s] %s\n", type.c_str(), id.c_str(), (isarray ? "array" : ""));
+	// }
+	void func_end() {
+		printf("	function end\n");
+	}
+
+	void print_start() {
+		printf("print start:\n");
+	}
+	void print_end() {
+		printf("	print end\n");
+	}
+	void input_start() {
+		printf("input start:\n");
+	}
+	void input_end() {
+		printf("	input end\n");
+	}
+	void if_start() {
+		printf("if start:\n");
+	}
+	void if_end() {
+		printf("	if end\n");
+	}
+	void while_start() {
+		printf("while start:\n");
+	}
+	void while_end() {
+		printf("	while end\n");
+	}
+	void return_start() {
+		printf("return start\n");
+	}
+	void return_end() {
+		printf("	return end\n");
+	}
+	void call_start(const string& id) {
+		printf("call start: [%s]\n", id.c_str());
+	}
+	void call_end() {
+		printf("	call end\n");
+	}
+	void set_start() {
+		printf("set start\n");
+	}
+	void set_end() {
+		printf("	set end\n");
+	}
+	void let_start() {
+		printf("let start\n");
+	}
+	void let_end() {
+		printf("	let end\n");
+	}
+
+	void ex_start() {
+		printf("expression start\n");
+	}
+	void ex_push(const string& ex) {
+		printf("	%s\n", ex.c_str());
+	}
+	void ex_end() {
+		printf("	expression end\n");
+	}
+	void varpath_start(const string& id) {
+		printf("varpath: [%s]\n", id.c_str());
+	}
+	void varpath_push(const string& id) {
+		printf("	%s\n", id.c_str());
+	}
+	void varpath_end() {
+		printf("	varpath end\n");
+	}
+};
+
+
+
+struct OutputB : Output {
+
+};
